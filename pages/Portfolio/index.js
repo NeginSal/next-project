b9039258efaba0f37340783b8ea3c819dec8b087
NeginSal <neginsaljooghi@gmail.com>
2022-03-21 20:20:22 +0330

    adding components

---
+++ pages/Portfolio/index.js
@@ -0,0 +1,9 @@
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>the portfolio page</h1>
+        </div>
+    );
+}
+
+export default Portfolio;
---
+++ pages/about/index.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return ( 
+        <div>
+            <h1>this is an about page</h1>
+        </div>
+     );
+}
+ 
+export default AboutPage;
