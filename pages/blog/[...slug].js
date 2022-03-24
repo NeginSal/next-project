9b224f6905459cbc0142eda93b54524df048f3f5
NeginSal <neginsaljooghi@gmail.com>
2022-03-24 12:46:22 +0430

    Navigation with link

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+const BlogPostsPage = () => {
+    const router = useRouter();
+    return ( 
+        <div>
+            <h1>The Blog Posts</h1>
+        </div>
+     );
+}
+ 
+export default BlogPostsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+
 const HomePage = () => {
     return ( 
         <div>
             <h1>this is homepage</h1>
+            <ul>
+                <li>
+                    <Link href="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link href="/clients">Clients</Link>
+                </li>
+
+            </ul>
         </div>
      );
 }
