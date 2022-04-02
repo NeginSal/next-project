6e235b49f4726e10958bc66c79d784bdea0b8242
NeginSal <neginsaljooghi@gmail.com>
2022-04-02 16:32:16 +0430

    add routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,9 +2,14 @@ import { useRouter } from "next/router";
 
 const ClinetProjectsPage = () => {
     const router = useRouter();
+    function LoadProjectHandler (){
+        //Load Data
+        router.push('./clients/max/projecta');
+    }
     return ( 
         <div>
             <h1>The project of given clinet</h1>
+            <button onClick={LoadProjectHandler}>Load Project A</button>
         </div>
      );
 }
