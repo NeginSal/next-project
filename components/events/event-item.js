3d726b09d57c2405df57e5df021edf002a6318cf
NeginSal <neginsaljooghi@gmail.com>
2022-04-06 11:31:00 +0430

    adding style

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import classes from './event-item.module.css'
 
 const EventItem = (props) => {
     const { title, image, date, location, id } = props;
@@ -8,22 +9,26 @@ const EventItem = (props) => {
         year: 'numeric'
     });
     const formattedAddress = location.replace(',', '\n');
-    const explorLink =`/events/${id}`;
+    const explorLink = `/events/${id}`;
     return (
-        <li>
+        <li className={classes.item}>
             <img src={'/' + image} alt="" />
-            <div>
-                <h2>{title}</h2>
-                <div>
-                    <time>{humanReadableDate}</time>
+            <div className={classes.content}>
+                <div className={classes.summary} >
+                    <h2>{title}</h2>
+                    <div className={classes.date}>
+                        <time>{humanReadableDate}</time>
+                    </div>
+                    <div className={classes.address}>
+                        <address>{formattedAddress}</address>
+                    </div>
                 </div>
-                <div>
-                    <address>{formattedAddress}</address>
+                <div className={classes.actions}>
+                    <Link href="/">{explorLink}</Link>
                 </div>
+
             </div>
-            <div>
-                <Link href="/">{explorLink}</Link>
-            </div>
+
 
         </li>
     );
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,9 +1,10 @@
 import  EventItem  from './event-item'
+import classes from './event-list.module.css'
 
 const EventList = (props) => {
     const { items } = props;
     return (
-        <ul>
+        <ul className={classes.list}>
             {items.map(event => <EventItem
                 key={event.id}
                 id={event.id}
