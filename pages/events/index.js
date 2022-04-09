c8aac243d9aeff66c3537228a4b33973ac7a3879
NeginSal <neginsaljooghi@gmail.com>
2022-04-09 16:24:17 +0430

    adding all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,14 @@
-const AllEventsPage = () => {
-    return ( 
-        <div>
-            <h1>All Events</h1>
-        </div>
-     );
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
+function AllEventsPage() {
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 }
- 
+
 export default AllEventsPage;
